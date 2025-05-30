71203e797c94338af76ddb7235fb994c42aaa4cb
2SSK <100ravsinghkarmwar@gmail.com>
2025-05-30 05:58:10 +0530

    feat: ✨ Solved Matrix Problem : "Rotate Image"

---
+++ Leetcode/matrix/rotate_image/main.cpp
@@ -0,0 +1,87 @@
+// ==================================
+//  You will input in the input.txt file
+//  Output will be printed in output.txt file
+// ==================================
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Utils {
+public:
+  template <typename T> vector<T> readInput() {
+    vector<T> inputArr;
+    string input;
+    getline(cin, input);
+    stringstream ss(input);
+    T value;
+    while (ss >> value) {
+      inputArr.push_back(value);
+    }
+
+    return inputArr;
+  }
+
+  template <typename T> void printArr(vector<T> arr) {
+    int n = arr.size();
+    for (int i = 0; i < n; i++) {
+      if (i == 0) {
+        cout << "[ " << arr[i] << " | ";
+      } else if (i == n - 1) {
+        cout << arr[i] << " ]\n";
+      } else {
+        cout << arr[i] << " | ";
+      }
+    }
+  }
+
+  template <typename T> void printArr(vector<vector<T>> arr) {
+    cout << "\n";
+    for (const auto &row : arr) {
+      printArr(row);
+    }
+  }
+};
+
+class Solution : public Utils {
+public:
+  void rotate(vector<vector<int>> &matrix);
+};
+
+int main() {
+  // Speed up input/output
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  Solution solution;
+
+  int n;
+  cin >> n;
+  cin.ignore();
+
+  vector<vector<int>> inputArr;
+  while (n--) {
+    vector<int> input = solution.readInput<int>();
+    inputArr.push_back(input);
+  }
+
+  solution.rotate(inputArr);
+
+  cout << "Output: ";
+  solution.printArr(inputArr);
+
+  return 0;
+}
+
+void Solution::rotate(vector<vector<int>> &matrix) {
+  int n = matrix.size();
+
+  for (int i = 0; i < n; i++) {
+    for (int j = i; j < n; j++) {
+      swap(matrix[i][j], matrix[j][i]);
+    }
+  }
+
+  for (int i = 0; i < n; i++) {
+    reverse(matrix[i].begin(), matrix[i].end());
+  }
+};
